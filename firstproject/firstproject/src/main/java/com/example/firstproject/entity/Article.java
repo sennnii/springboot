14bded83b8d2ac2a141be96efbf0e6d640ca791b
Park Seeun <park.seeun@gmail.com>
2024-09-21 18:00:43 +0900

    12강 완료

---
+++ firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,26 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity  //db가 해당 객체를 인식 가능
+@AllArgsConstructor
+@ToString
+@NoArgsConstructor //디폴트 생성자 추가
+public class Article {
+    @Id//대표값을 지정! like a 주민등록번호
+    @GeneratedValue  //1,2,3,4,.....자동 생성 어노테이션!
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+}
