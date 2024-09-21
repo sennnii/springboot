5189f8f1f33817fd3e52f5cb920357c59660acf1
Park Seeun <park.seeun@gmail.com>
2024-09-21 18:34:24 +0900

    13강 링크와 리다이렉트

--- firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
@@ -12,6 +13,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @NoArgsConstructor //디폴트 생성자 추가
+@Getter
 public class Article {
     @Id//대표값을 지정! like a 주민등록번호
     @GeneratedValue  //1,2,3,4,.....자동 생성 어노테이션!
@@ -22,5 +24,6 @@ public class Article {
 
     @Column
     private String content;
+
 
 }
